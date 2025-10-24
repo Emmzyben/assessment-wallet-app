7bc7972891a2dd70a329d985fecf8d67f7e5a7d7
unknown <emmco96@gmail.com>
2025-10-24 19:48:34 +0100

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'wallet-app',
+  description: 'Created by Emmanuel Amadi',
+  generator: 'wallet-app',
 }
 
 export default function RootLayout({
